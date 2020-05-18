1a660e511c0b108addeb932f10db6a21a7f6a8ce
Golubkov Nikita <nigolubkov@gmail.com>
2020-05-18 13:12:41 +0300

    Оптимизирует адаптивную графику под ретину

--- source/js/dropdown-alphabet.js
+++ source/js/dropdown-alphabet.js
@@ -2,5 +2,6 @@ var select = document.querySelector(".steps__select-btn--choose");
 var minAlphabet = document.querySelector(".steps__alphabet-wrapper");
 
 select.addEventListener("click", function(){
+  select.classList.toggle("steps__select-btn--blue");
   minAlphabet.classList.toggle("is-show");
 })
