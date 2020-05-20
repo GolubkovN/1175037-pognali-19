e22145d6f2ddffa0b64ab0f2d72197a8035320fe
Golubkov Nikita <nigolubkov@gmail.com>
2020-05-21 01:57:17 +0300

    добавляет эфекты по стайлгайду

--- source/js/dropdown-alphabet.js
+++
@@ -1,7 +0,0 @@
-var select = document.querySelector(".steps__select-btn--choose");
-var minAlphabet = document.querySelector(".steps__alphabet-wrapper");
-
-select.addEventListener("click", function(){
-  select.classList.toggle("steps__select-btn--blue");
-  minAlphabet.classList.toggle("is-show");
-})
